81ef3e04be88804ddc3e257525ed410742b54d89
yoodye <110244383+yoodye@users.noreply.github.com>
2022-10-07 12:00:01 -0300

    Aula 07.10

--- estudo.js
+++ estudo.js
@@ -1,30 +1,13 @@
-let tabuada = 2;
-
-function escreva() {
-document.write (tabuada + " x 1 = "+(tabuada*1)+"<br>");
-document.write (tabuada + " x 2 = "+(tabuada*2)+"<br>");
-document.write (tabuada + " x 3 = "+(tabuada*3)+"<br>");
-document.write (tabuada + " x 4 = "+(tabuada*4)+"<br>");
-document.write (tabuada + " x 5 = "+(tabuada*5)+"<br>");
-document.write (tabuada + " x 6 = "+(tabuada*6)+"<br>");
-document.write (tabuada + " x 7 = "+(tabuada*7)+"<br>");
-document.write (tabuada + " x 8 = "+(tabuada*8)+"<br>");
-document.write (tabuada + " x 9 = "+(tabuada*9)+"<br>");
-document.write (tabuada + " x 10 = "+(tabuada*10)+"<br>");
+function total(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+        document.write("Mês " + i + "valor:" + r +"<br>");
+        c = r;
+    }
+    
+    document.write("Resultado: " + r);
 }
-
-function repete(){
-    for(let i = 1; i <= 10; i++){
-     document.write("Tabuada do " + i + "<br>")
-        for(let j =1; j <= 10; j++){
-            document.write (tabuada + " x " + j + " = "+(j*i)+"<br>")
-        }
-        document.write ("<br>")
-     }
-}
-
-  let frutas = ["Amora","Uva","Abacate","Maçã","Goiaba","Jabuticaba"];
-
-  function minhaLista(){
-      document.write(frutas[3]);
-  }
