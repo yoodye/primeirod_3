5c2c89eb4c86ff3f83dede625a6b2617b6bccfaf
yoodye <110244383+yoodye@users.noreply.github.com>
2022-10-21 12:01:39 -0300

    Aula 21-10

--- estudo.js
+++ estudo.js
@@ -1,14 +1,38 @@
+function moeda(atual){
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+
 function total(){
     let c = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if (!Number(c)){
+       alert("O Capital deve ser números.");
+       document.getElementById("valor").value = "";
+       document.getElementById("valor").focus();
+       return
+    }
+    if (!Number(j)){
+      alert("O Capital deve ser números.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return
+    }
+    if (!Number(t)){
+      alert("O Capital deve ser números.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+      return
+    }
+    
+  
     let r = 0;
     for(let i = 1; i <= t; i++){
         r = c * (1 + (j/100));
-        document.write("Mês " + i + " valor: " + r +"<br>");
+        document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
         c = r;
     }
-    document.write("Resultado: " + r);
+    document.write("Resultado: " + moeda(r));
   }
   
   
